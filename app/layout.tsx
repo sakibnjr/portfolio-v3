89d6f37e5a96e7198ce79d516410af5f96336cee
Sakib Nahid <sakibnjr@proton.me>
2026-08-27 20:00:55 +0600

    cleaned up everything

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/app/_components/Header";
-import Footer from "@/app/_components/Footer";
 
 export const metadata: Metadata = {
   title: {
@@ -9,12 +7,6 @@ export const metadata: Metadata = {
     default: "Sakib Nahid developer portfolio",
   },
   description: "Website of Sakib Nahid, a software engineer and web developer.",
-  openGraph: {
-    title: "Sakib Nahid",
-    description:
-      "Website of Sakib Nahid, a software engineer and web developer.",
-    url: "",
-  },
 };
 
 export default function RootLayout({
@@ -23,14 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`antialiased max-w-7xl mx-auto px-8`}>
-      <body>
-        <Header />
-
-        {children}
-
-        <Footer />
-      </body>
+    <html lang="en" className="antialiased">
+      <body></body>
     </html>
   );
 }
